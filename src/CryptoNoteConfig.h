210d7d6c3d3393ce0729955f3db4ccfa86105eea
yagamidev <yagamidev@gmail.com>
2017-11-24 13:04:48 +0100

    Changing genesis coinbase

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -108,6 +108,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 const std::initializer_list<const char*> SEED_NODES = {
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
+  "127.0.0.1:8080"  
 };
 
 struct CheckpointData {
